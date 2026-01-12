189910fae38d209e12b2b0a576f3e99c4a00ed4a
kasim0616 <nihal1662001@gmail.com>
2026-01-12 03:32:36 +0100

    changes after deployment

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 
 import { Navbar, Footer, Landing, About, Skills, Testimonials, Blog, Education, Experience, Contacts, Projects, Achievement } from '../../components'
-import { headerData } from '../../data/headerData'
 
 function Main() {
     return (
