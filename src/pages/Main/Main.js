f5d80841af9ff894c5b22c17efd7acc3ee03f16c
kasim0616 <nihal1662001@gmail.com>
2026-01-13 00:40:14 +0100

    changes

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 
-import { Navbar, Footer, Landing, About, Skills, Testimonials, Blog, Education, Experience, Contacts, Projects, Achievement } from '../../components'
+import { Navbar, Footer, Landing, About, Skills, Testimonials, Education, Experience, Contacts, Projects, Achievement } from '../../components'
+import { headerData } from '../../data/headerData'
 
 function Main() {
     return (
         <div>
             <Helmet>
-                <title>My Portfolio</title>
+                <title>Sanmathi | Portfolio</title>
             </Helmet>
 
             <Navbar />        
