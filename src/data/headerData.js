3cc80cb62d0c2135aba6122e287aed3d4da794df
kasim0616 <nihal1662001@gmail.com>
2026-01-12 03:20:52 +0100

    bugs fixed

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,12 @@
+import profileImage from '../assets/image/profile.jpeg';
+import resume from '../assets/pdf/san resume.pdf';
+
 export const headerData = {
     name: 'Hi! I am San',
     signature: 'sanmathi parameswaran',
     title: 'Water Engineer',
     desciption:
         "I'm a 24-year-old early-career engineer with experience across India, Germany, and Norway, working at the intersection of water engineering, applied research, and data analysis. I'm interested in real problems, real systems, and solutions that actually work outside the lab.",
-    image: '',
-    resumePdf: '',
+    image: profileImage,
+    resumePdf: resume,
 };
